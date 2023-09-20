9122dd35ba17fd07694982d0e361432b252f410a
Sarnavskiy Alexey <alexey.sarnavskiy@ya.ru>
2023-09-20 18:32:20 +0300

    Added empty files for Module 29

---
+++ Module_29/Module_29.cpp
@@ -0,0 +1,25 @@
+// #ifdef __MODULE_29__
+#include <iostream>
+#include "Module_29.hpp"
+
+void Module_29()
+{
+    std::cout << "\n\t\t\t\tМодуль 29.\n";
+    unsigned short userAnswer;
+
+    do {
+        std::cout << "\tВведите номер задания 1-2 или 0 для выхода: ";
+        std::cin >> userAnswer;
+
+        switch (userAnswer) {
+        case 0: break;
+        case 1: Task_29_1(); break;
+        case 2: Task_29_2(); break;
+        default: std::cout << "\tЗадание в ДЗ отсутствует. Выберите другое задание!\n";
+        }
+
+        std::cout << equals;
+
+    } while (userAnswer != 0);
+}
+// #endif
--- main.cpp
+++ main.cpp
@@ -92,6 +92,9 @@ int main() {
             #ifdef __MODULE_28__
             case 28: Module_28(); break;
             #endif
+            #ifdef __MODULE_29__
+            case 29: Module_29(); break;
+            #endif
             default: {
                 std::cout << "ДЗ по модулю отсутствует либо выставлены неправильные флаги компиляции"
                           << ".\nВыберите другой модуль либо пересоберите проект!\n";
